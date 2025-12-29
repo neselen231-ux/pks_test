fd1e0cc101563aa91af515fe344db05be77f89b4
neselen231-ux <neselen231@gmail.com>
2025-12-29 10:06:31 +0100

    Update st_test_v2.py

--- pks_v2/st_test_v2.py
+++ pks_v2/st_test_v2.py
@@ -1,14 +1,27 @@
 import streamlit as st
+from sqlalchemy import create_engine, text
+import pandas as pd
+
+engine = create_engine(
+    f"mysql+pymysql://{st.secrets['DB_USER']}:{st.secrets['DB_PASS']}@{st.secrets['DB_HOST']}:{st.secrets['DB_PORT']}/{st.secrets['DB_NAME']}",
+    connect_args={
+        "ssl": {"ca": "ca.pem"}
+    }
+)
 
 st.title("PKS Reception")
 
 # 입력란 2개
-name = st.number_input("Reference number")
-age = st.number_input("quantity", min_value=0, step=1)
+reference = st.number_input("Reference number")
+qty = st.number_input("quantity", min_value=0, step=1)
 
-# 버튼
 if st.button("Input"):
-    if name:
-        st.success(f"REF : {name} & QTY : {age}")
+    if reference:
+        with engine.begin() as conn: 
+            conn.execute(
+                text("INSERT INTO reception (name, age) VALUES (:ref, :qty)"),
+                {"ref": int(reference), "qty": int(qty)}
+            )
+        st.success("DB updated")
     else:
-        st.warning("Please input reference")
+        st.warning("Reference missing")
