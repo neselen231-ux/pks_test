33e9c69c9386ed827c5732d520d3c4b2c1660eec
neselen231-ux <neselen231@gmail.com>
2026-01-06 23:13:30 +0100

    Update stock.py

--- pks_v2/stock.py
+++ pks_v2/stock.py
@@ -23,14 +23,19 @@ engine = create_engine(
         "ssl": {"ca": "ca.pem"}
     }
 )
+st.title("PFE stock system")
+
+st.subheader("Lots to stock")
 df = pd.read_sql("SELECT * FROM reception", con=engine)
+st.table(df[(df["Emplacement"].isna()) & (df["Lot_number"].notna())])
+
 if "changed_lots" not in st.session_state:
     st.session_state["changed_lots"] = []
 
 lot = st.number_input("lot to stock",min_value=0)
 emplacement = st.text_input("Emplactement")
 
-st.table(df[(df["Emplacement"].isna()) & (df["Lot_number"].notna())])
+
 
 if st.button("stock input"):
     with engine.begin() as conc:
@@ -42,6 +47,7 @@ if st.button("stock input"):
 
 table = df[df["Lot_number"].isin(st.session_state["changed_lots"])]
 
+st.subheader("Stocked lots")
 if st.session_state["changed_lots"]:
     df = pd.read_sql(
         text("SELECT * FROM reception"),
