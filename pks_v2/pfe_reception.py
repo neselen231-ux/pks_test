63d1cf15a74e4ab21eedb9dd357637ca2c95fd7a
neselen231-ux <neselen231@gmail.com>
2026-01-22 23:05:24 +0100

    Update pfe_reception.py

--- pks_v2/pfe_reception.py
+++ pks_v2/pfe_reception.py
@@ -144,8 +144,8 @@ if st.button("Input"):
                             total_h = ref_img.height + lot_img.height
 
                             combined = Image.new("RGB", (max_w, total_h), "white")
-                            combined.paste(ref_img, (150, 40))
-                            combined.paste(lot_img, (150, ref_img.height))
+                            combined.paste(ref_img, (150, 20))
+                            combined.paste(lot_img, (150, ref_img.height+10))
 
                             text_sticker = ImageDraw.Draw(combined)
                             text_sticker.text(
@@ -189,8 +189,8 @@ if st.button("Input"):
                         font=ffont
                     )
 
-                    combined.paste(ref_img, (150, 40))
-                    combined.paste(lot_img, (150, ref_img.height))
+                    combined.paste(ref_img, (150, 20))
+                    combined.paste(lot_img, (150, ref_img.height+10))
 
 
                     download_buffer = BytesIO()
@@ -240,6 +240,7 @@ new_rows = df.iloc[-10:,[-1,0,1,2]]
 
 with st.expander("last 10 receptions",expanded=False):
     st.table(new_rows)
+
 
 
 
