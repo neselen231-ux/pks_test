cafbc95ffc735582b10fac65a1add99af8aa9c89
neselen231-ux <neselen231@gmail.com>
2026-01-22 20:48:30 +0100

    Update pfe_reception.py

--- pks_v2/pfe_reception.py
+++ pks_v2/pfe_reception.py
@@ -140,7 +140,7 @@ if st.button("Input"):
                     max_w = max(ref_img.width, lot_img.width)
                     total_h = ref_img.height + lot_img.height
 
-                    combined = Image.new("RGB", (max_w*1.3, total_h*1.3), "white")
+                    combined = Image.new("RGB", (max_w, total_h), "white")
                     combined.paste(ref_img, (0, 0))
                     combined.paste(lot_img, (0, ref_img.height))
 
@@ -201,6 +201,7 @@ new_rows = df.iloc[-10:,:3]
 
 with st.expander("last 10 receptions",expanded=False):
     st.table(new_rows)
+
 
 
 
