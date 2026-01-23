1a7156e2415c84c33c56159dd239f674f1892181
neselen231-ux <neselen231@gmail.com>
2026-01-23 08:29:07 +0100

    Update pfe_reception.py

--- pks_v2/pfe_reception.py
+++ pks_v2/pfe_reception.py
@@ -184,7 +184,7 @@ if st.button("Input"):
                     text_sticker = ImageDraw.Draw(combined)
                     text_sticker.text(
                         (115, 0),
-                        f"PFE Reception: {dt.datetime.now().date()}",
+                        f"{dt.datetime.now().date()}_{delivery_note}",
                         fill="black",
                         font=ffont
                     )
@@ -240,6 +240,7 @@ new_rows = df.iloc[-10:,[-1,0,1,2]]
 
 with st.expander("last 10 receptions",expanded=False):
     st.table(new_rows)
+
 
 
 
