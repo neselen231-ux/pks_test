1d3fb55e30757e2140dcbb59afc937a686aa5bfc
neselen231-ux <neselen231@gmail.com>
2026-01-27 22:27:23 +0100

    Update pfe_reception.py

--- pks_v2/pfe_reception.py
+++ pks_v2/pfe_reception.py
@@ -132,10 +132,15 @@ if st.button("Input"):
                             if sup_lot:
                                 Code128(f"{sup_lot}_{i}", writer=ImageWriter()).write(sup_lots, options)
                                 filename = f"{sup_lot}_{i}_{reference}_barcodes.png"
+                                this_lot = OP_lots[i - 1]  ### ✅ FIX: i번째 row의 OP_lot 사용
+                                Code128(f"{this_lot}", writer=ImageWriter()).write(buf_lot, options)
+                                filename = f"{this_lot}_{reference}_barcodes.png"  ### ✅ FIX
 
                             else:
                                 if not sup_lot:
                                     sup_lot="NA"
+                                Code128(f"{sup_lot}_{i}", writer=ImageWriter()).write(sup_lots, options)
+                                filename = f"{sup_lot}_{i}_{reference}_barcodes.png"
                                 this_lot = OP_lots[i - 1]  ### ✅ FIX: i번째 row의 OP_lot 사용
                                 Code128(f"{this_lot}", writer=ImageWriter()).write(buf_lot, options)
                                 filename = f"{this_lot}_{reference}_barcodes.png"  ### ✅ FIX
@@ -287,6 +292,7 @@ new_rows = df.iloc[-10:,[-1,0,1,2]]
 
 with st.expander("last 10 receptions",expanded=False):
     st.table(new_rows)
+
 
 
 
