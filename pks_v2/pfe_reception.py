638c37aabe97297cf92434c9401042e717635463
neselen231-ux <neselen231@gmail.com>
2026-01-28 11:36:10 +0100

    Update pfe_reception.py

--- pks_v2/pfe_reception.py
+++ pks_v2/pfe_reception.py
@@ -37,6 +37,7 @@ qty = st.number_input("quantity", min_value=0, step=1)
 delivery_note = st.text_input("Delivery note",max_chars=20)
 sup_lot = st.text_input("Supplier lot",max_chars=40)
 Comment = st.text_input("Comment",max_chars=20)
+project = st.text_input("Project",max_chars=8)
 
 
 
@@ -175,7 +176,7 @@ if st.button("Input"):
                             text_sticker = ImageDraw.Draw(combined)
                             text_sticker.text(
                                 (105, 0),
-                                f"{dt.datetime.now().date()}",
+                                f"{dt.datetime.now().date()}   proejct : {project}",
                                 fill="black",
                                 font=ffont
                             )
@@ -306,6 +307,7 @@ new_rows = df.iloc[-10:,[-1,0,1,2]]
 
 with st.expander("last 10 receptions",expanded=False):
     st.table(new_rows)
+
 
 
 
