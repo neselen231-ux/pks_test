695a9253bdce535fd940cdb347a9529b6575dd5c
neselen231-ux <neselen231@gmail.com>
2026-02-05 14:36:33 +0100

    Update pfe_download.py

--- pks_v2/pfe_download.py
+++ pks_v2/pfe_download.py
@@ -2,4 +2,26 @@ import datetime as dt
 import streamlit as st
 import pandas as pd
 
+
+hide_ui = """
+<style>
+#MainMenu {visibility: hidden;}      /* 메뉴 */
+header {visibility: hidden;}         /* 헤더 */
+footer {visibility: hidden;}         /* Footer */
+
+div[data-testid="stStatusWidget"] {display: none;}   /* status badge */
+div[data-testid="stDecoration"] {display: none;}     /* hosted badge */
+div.viewerBadge_link__1S137 {display: none;}         /* created by */
+</style>
+"""
+st.markdown(hide_ui, unsafe_allow_html=True)
+
+engine = create_engine(
+    f"mysql+pymysql://{st.secrets['DB_USER']}:{st.secrets['DB_PASS']}@{st.secrets['DB_HOST']}:{st.secrets['DB_PORT']}/{st.secrets['DB_NAME']}",
+    connect_args={
+        "ssl": {"ca": "ca.pem"}
+    }
+)
+st.title("Reception download")
+
 st.button("reception history download",download=)
