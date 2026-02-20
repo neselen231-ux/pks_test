3fda7b3f20703856787266637fd593a4086b1d8a
neselen231-ux <neselen231@gmail.com>
2026-02-20 09:20:25 +0100

    Update pfe_reception.py

--- pks_v2/pfe_reception.py
+++ pks_v2/pfe_reception.py
@@ -208,7 +208,7 @@ if submit:
                     font=ffont2
                 )
                 text_sticker.text(
-                    (300, ref_img.height+145),
+                    (200, ref_img.height+125),
                     f"{usage}",
                     fill="black",
                     font=ffont2
@@ -283,7 +283,7 @@ if submit:
                                 font=ffont2
                             )
                             text_sticker.text(
-                                (300, ref_img.height+145),
+                                (200, ref_img.height+125),
                                 f"{usage}",
                                 fill="black",
                                 font=ffont2
@@ -348,6 +348,7 @@ new_rows = df.iloc[-10:,[-2,0,1,2]]
 
 with st.expander("last 10 receptions",expanded=False):
     st.table(new_rows)
+
 
 
 
