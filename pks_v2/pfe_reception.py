2f646f1d7a622ad93e4332733de12ae7517e52c3
neselen231-ux <neselen231@gmail.com>
2026-02-23 16:41:18 +0100

    Update pfe_reception.py

--- pks_v2/pfe_reception.py
+++ pks_v2/pfe_reception.py
@@ -224,7 +224,7 @@ if submit:
                             total_h = ref_img.height + lot_img.height + 20
 
                             combined = Image.new("RGB", (max_w, total_h), "white")
-                            combined.paste(lot_img, (80, ref_img.height+15))
+                            combined.paste(lot_img, (50, ref_img.height+15))
 
                             text_sticker = ImageDraw.Draw(combined)
                             text_sticker.text(
@@ -305,6 +305,7 @@ new_rows = df.iloc[-10:,[-2,0,1,2]]
 
 with st.expander("last 10 receptions",expanded=False):
     st.table(new_rows)
+
 
 
 
