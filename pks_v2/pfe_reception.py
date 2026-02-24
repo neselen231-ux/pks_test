8f1ffadfb5a42be8a984e740877777ba00f3f52d
neselen231-ux <neselen231@gmail.com>
2026-02-24 09:26:02 +0100

    Update pfe_reception.py

--- pks_v2/pfe_reception.py
+++ pks_v2/pfe_reception.py
@@ -145,7 +145,7 @@ if submit:
                 dm_barcode = treepoem.generate_barcode(barcode_type="datamatrix",data=data)
                 
                 dm_img = dm_barcode.convert("RGB")
-                dm_img = dm_img.resize((1200, 1200), Image.NEAREST)
+                dm_img = dm_img.resize((700, 700), Image.NEAREST)
                 ##########################################""
 
                 
@@ -317,6 +317,7 @@ new_rows = df.iloc[-10:,[-2,0,1,2]]
 
 with st.expander("last 10 receptions",expanded=False):
     st.table(new_rows)
+
 
 
 
