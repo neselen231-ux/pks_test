dcf1be0b787a3533030e075df3ecb9ff41496b1d
neselen231-ux <neselen231@gmail.com>
2026-02-25 11:35:00 +0100

    Update pfe_reception.py

--- pks_v2/pfe_reception.py
+++ pks_v2/pfe_reception.py
@@ -144,6 +144,19 @@ if submit:
                 GS = chr(29)
                 EOT = chr(4)
 
+                    data = (
+                    "[)>" +
+                    RS +
+                    GS +
+                    f"S{OP_lot}" +
+                    GS +
+                    f"V{vendor}" +
+                    GS +
+                    f"P{reference.upper()}" +
+                    GS +
+                    f"V{vendor}" +
+                    RS
+                
                 data = f"S{OP_lot}\nV{vendor}\nP{reference.upper()}\nQ{qty}"
 
                 dm_barcode = treepoem.generate_barcode(barcode_type="datamatrix",data=data)
@@ -316,6 +329,7 @@ new_rows = df.iloc[-10:,[-2,0,1,2]]
 
 with st.expander("last 10 receptions",expanded=False):
     st.table(new_rows)
+
 
 
 
