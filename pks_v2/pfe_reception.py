77762f693b36849e17fd7dad7f1c0bdf57bbb77c
neselen231-ux <neselen231@gmail.com>
2026-02-25 11:53:25 +0100

    Update pfe_reception.py

--- pks_v2/pfe_reception.py
+++ pks_v2/pfe_reception.py
@@ -144,18 +144,7 @@ if submit:
                 GS = chr(29)
                 EOT = chr(4)
 
-                    data = (
-                    "[)>" +
-                    RS +
-                    GS +
-                    f"S{OP_lot}" +
-                    GS +
-                    f"V{vendor}" +
-                    GS +
-                    f"P{reference.upper()}" +
-                    GS +
-                    f"V{vendor}" +
-                    RS
+                data = "[)>" + RS + GS*5 + f"S{OP_lot}" + GS + f"V{vendor}" + GS + f"P{reference.upper()}" + GS + f"V{vendor}" + RS
                 
                 data = f"S{OP_lot}\nV{vendor}\nP{reference.upper()}\nQ{qty}"
 
@@ -329,6 +318,7 @@ new_rows = df.iloc[-10:,[-2,0,1,2]]
 
 with st.expander("last 10 receptions",expanded=False):
     st.table(new_rows)
+
 
 
 
